0715a81f44938b3868b9ae0e5801bf939dc104e3
Luciano <luc.oliveira343@gmail.com>
2023-03-28 01:43:39 -0300

    adicioando a configuração edas variaveis de ambiente do Java

--- Arch-Linux/jdk.sh
+++ Arch-Linux/jdk.sh
@@ -4,3 +4,7 @@
 sudo pacman -Syu
 sudo pacman -S jre17-openjdk
 sudo pacman -S jdk17-openjdk
+
+
+echo "export JAVA_HOME=/usr/lib/jvm/java-17-openjdk" >> /home/$USER/.bashrc
+echo "export PATH=\$PATH:\$JAVA_HOME/bin" >> /home/$USER/.bashrc
