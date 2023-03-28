e7ba8e7adff136e9693b9a1d447000193da6ca32
Luciano <luc.oliveira343@gmail.com>
2023-03-28 00:56:35 -0300

    adicionando a instalação do Arch Linux

--- Ubuntu/docker.sh
+++ Ubuntu/docker.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-sudo apt update -y
-sudo apt install docker.io -y
+sudo pacman -Syu
+sudo pacman -S docker
+sudo systemctl start docker.service
+sudo systemctl enable docker.service
 sudo usermod -aG docker $USER
+## sudo systemctl status docker.service
